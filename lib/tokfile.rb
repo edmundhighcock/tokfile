614ad92ff138631d68e78e470f9c346933f7d210
Edmund Highcock <edmundhighcock@users.sourceforge.net>
2014-04-01 17:11:17 +0100

    Command line interface working

--- lib/tokfile.rb
+++ lib/tokfile.rb
@@ -26,44 +26,54 @@ SYNOPSIS
 
 DESCRIPTION
 	
-  This test utility is written to test the command-line-flunky gem.
+  Convert, manipulate and display files containing tokamak-like data, for example
+  EQDSK, tokamak profile database, iterdb output files from one-two etc. 
   
 EXAMPLES
 
-   $ test_utility hello_world
+  $ tokfile convert EQDSK ogyropsi.dat -f eqdsk,ogyropsi
+
+  $ tokfile merge ogyropsi.dat,ogyropsi2.dat ogyropsi3.dat -f ogyropsi -m profiles,geometry
+
+  $ tokfile disp EQDSK -f eqdsk 
    
-   $ test_utility test_bool -b
    
 EOF
 	
 	COMMANDS_WITH_HELP = [
-		['hello_world', 'hello', 0, 'Say hello to the world', [], []],
-		['test_bool', 'tbool', 0,  'Test whether the boolean flag works', [], [:b]],
-		['test_arguments', 'args', 1,  'Test passing an argument to a command and an argument to an option', ['command_argument'], [:a]],
+		['convert', 'cv', 2,  'Convert files of one format into another. Not all file formats contain the same information. If information is missing from the input files, a warning will be printed and tokfile will attempt to fill in the gaps using sensible assumptions like Ti/Te = 1. More than one output file can be specified.', ['inputfile', 'outputfile(s)'], [:f, :t]],
+		['display_summary_graph', 'disp', 1,  'Display a summary graph of the file using gnuplot.', ['inputfile',], [:f]],
+		['merge', 'mg', 2,  'Merge data from two input files to create a single output file. ', ['inputfile', 'outputfile(s)'], [:f, :t, :m]],
+		['write_summary_graph', 'wsg', 2,  'Write a summary graph of the file to disk using gnuplot. The file format is determined by the extension of the graph file', ['inputfile', 'graph file'], [:f]],
 
 	]
 	
+	SUPPORTED_FORMATS = ['eqdsk', 'ogyropsi']
+	SUPPORTED_MERGE_SOURCES = ['profiles', 'geometry']
+
+
 	COMMAND_LINE_FLAGS_WITH_HELP = [
-		['--boolean', '-b', GetoptLong::NO_ARGUMENT, 'A boolean option'],		
-		['--argument', '-a', GetoptLong::REQUIRED_ARGUMENT, 'An option which requires an argument '],		
+		#['--boolean', '-b', GetoptLong::NO_ARGUMENT, 'A boolean option'],		
+		['--formats', '-f', GetoptLong::REQUIRED_ARGUMENT, "A list of formats pertaining to the various input and output files (in the order which they appear), separated by commas. If they are all the same, only one value may be given. If a value is left empty (i.e. there are two commas in a row) then the previous value will be used. Currently supported formats are #{SUPPORTED_FORMATS.inspect}. "],		
+		['--merge-sources', '-m', GetoptLong::REQUIRED_ARGUMENT, "A list of which bits of information should come from which input file during a merge. Currently supported merge sources are #{SUPPORTED_MERGE_SOURCES.inspect} "],		
 
 		]
 
 	LONG_COMMAND_LINE_OPTIONS = [
-	["--no-short-form", "", GetoptLong::NO_ARGUMENT, %[This boolean option has no short form]],
+	#["--no-short-form", "", GetoptLong::NO_ARGUMENT, %[This boolean option has no short form]],
 	] 
 		
 	# specifying flag sets a bool to be true
 
-	CLF_BOOLS = [:b, :no_short_form]
+	CLF_BOOLS = []
 
 	CLF_INVERSE_BOOLS = [] # specifying flag sets a bool to be false
 	
-	PROJECT_NAME = 'command_line_flunky_test_utility'
+	PROJECT_NAME = 'tokfile'
 		
 	def self.method_missing(method, *args)
 # 		p method, args
-		CommandLineFlunkyTestUtility.send(method, *args)
+		TokFile.send(method, *args)
 	end
 	
 	#def self.setup(copts)
@@ -73,18 +83,8 @@ EOF
 	SCRIPT_FILE = __FILE__
 end
 
-class CommandLineFlunkyTestUtility
+class TokFile
 	class << self
-		def hello_world(copts)
-			puts "Hello World"
-		end
-		def test_bool(copts)
-			puts "Bool is #{copts[:b]}"
-			puts "no-short-form is #{copts[:no_short_form]}"
-		end
-		def test_arguments(argument, copts)
-			puts "command argument was #{argument} and option argument was #{copts[:a]}"
-		end
 		# This function gets called before every command
 		# and allows arbitrary manipulation of the command
 		# options (copts) hash
